98433cabae8d16a14f2e16db42e3321e5e62e15d
Keith Johnson <keithjohnson@zbly.org>
2016-11-03 18:25:29 +0000

    Removed the creation of players

--- app/controllers/tourney_sets_controller.rb
+++ app/controllers/tourney_sets_controller.rb
@@ -1,7 +1,5 @@
 class TourneySetsController < ApplicationController
   def new
-    @player1 = Player.new("Ryu")
-    @player2 = Player.new("Ken")
     @tourney_set = TourneySet.new(@player1, @player2)
     render 'tourney_sets/new'
   end
