aaea5b6dcc60a141b2568fdbb121035d494a0c6e
Keith Johnson <keithjohnson@zbly.org>
2016-11-06 22:19:04 +0000

    Added players and tourney_sets as resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tourney_sets#new'
+  resources :players
+  resources :tourney_sets
 end
