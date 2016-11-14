2a9f1b10ffa422ac69a93af61d677fa5235d2800
Keith Johnson <keithjohnson@zbly.org>
2016-11-14 04:02:39 +0000

    Added AddPlayerRefToBrackets

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -19,6 +19,7 @@ class BracketsController < ApplicationController
 
   # GET /brackets/1/edit
   def edit
+    
   end
 
   # POST /brackets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113213207) do
+ActiveRecord::Schema.define(version: 20161114031856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20161113213207) do
   create_table "brackets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "player_id"
+    t.index ["player_id"], name: "index_brackets_on_player_id", using: :btree
   end
 
   create_table "players", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 20161113213207) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "brackets", "players"
 end
